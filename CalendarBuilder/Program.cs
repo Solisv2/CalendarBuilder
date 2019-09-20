f7ef99ba0347aa4967c9a3d1cbdc9327a6b7d597
Victor Ortiz <solisv2@yahoo.com>
2019-09-20 13:43:08 -0500

    commit 1

---
+++ CalendarBuilder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarBuilder
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DataBuilder db = new DataBuilder();
+            db.StartCount = 6;
+            db.EndCount = 31;
+            Console.WriteLine(db.GetData());
+
+
+        }
+    }
+    public class DataBuilder
+    {
+        public int EndCount { get; set; }
+        public int StartCount { get; set; }
+        public string GetData()
+        {
+            StringBuilder sb = new StringBuilder();
+            //StringBuilder sb = new StringBuilder($"{StartCount}", 20);
+            //int l = StartCount;
+            //while (l<=EndCount)
+            //{
+            //    sb.Append(" ");
+            //    l++;
+            //    sb.Append(l);
+            //}
+            int ModAns=0;
+            for (int i = StartCount; i <= EndCount; i++)
+            {
+                sb.Append($"{i, 5}");
+                sb.Append(" ");
+                ModAns = i % 7;
+                if (ModAns == 0)
+                {
+                    sb.AppendLine();
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
